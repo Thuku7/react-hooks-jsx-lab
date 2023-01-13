bbe0db5cd45340779c33ce19ae1fa6ce28064692
Robert <robethuku11@gmail.com>
2023-01-13 17:15:13 +0300

    Add a h1 element and its content

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,13 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div id="home"></div>;
+  
+  return (
+    <div id="home">
+      <h1 style="color: fireBrick">{name} is a Web Developer from {city}</h1>
+    </div>
+
+  )
 }
 
 export default Home;
